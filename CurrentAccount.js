8701c34bb513279a2381ee82b6f760ae69301811
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-10-02 21:26:24 -0300

    finished

--- CurrentAccount.js
+++ CurrentAccount.js
@@ -1,8 +1,28 @@
-export class CurrentAccount{
-    agency
-    client
+import { Client } from "./Client.js"
 
-    #balance
+export class CurrentAccount{
+    static count = 0
+    agency
+
+    _client
+    get client(){
+        return this._client
+    }
+    set client(newValue){
+        if(newValue instanceof Client)
+        this._client = newValue
+    }
+
+    _balance = 0
+    get balance(){
+        return this._balance
+    }
+
+    constructor(agency, client){
+        this.agency = agency
+        this._client = client
+        CurrentAccount.count++
+    }
 
     withdraw(value){
         if(this._balance >= value && value > 0)
